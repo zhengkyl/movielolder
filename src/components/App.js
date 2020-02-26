7bbf3a423124b62cc5f973cadef891b52264c8cb
Kyle Zheng <kylezheng73@gmail.com>
2020-02-26 02:55:40 -0500

    basic layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,106 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import movie from '../images/movie.jpg'
+import movie from "../images/movie.jpg";
 
-import { Card, CardMedia, makeStyles } from '@material-ui/core';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Container,
+  Paper,
+  Grid,
+  makeStyles
+} from "@material-ui/core";
 
 const useStyles = makeStyles({
-  root: {
-
+  root: {},
+  card: {
+    height: 300,
+    width:200,
   },
-  card:{
-    maxWidth:300,
+  cardInfo: {
+    height: 300,
+    width:300,
   },
-  media:{
-    height:300,
+  media: {
+    height: 300,
+    width: 200
   }
-})
-
+});
 
 function App() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <div className="App">
-      <Card className={classes.card}>
-        <CardMedia>
-          <img src={movie} className={classes.media} alt="henlo"></img>
-        </CardMedia>
+      <Grid
+        container
+        spacing={6}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh", maxWidth: "100vw" }}
+      >
+        <Grid
+          container
+          item
+          spacing={4}
+          direction="row"
+          alignItems="center"
+          justify="center"
+        >
+          <Grid item>
+            <Card className={classes.card} raised={true}>
+              <CardMedia>
+                <img src={movie} className={classes.media} alt="henlo"></img>
+              </CardMedia>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card className ={classes.cardInfo}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
 
-      </Card>
+        <Grid
+          container
+          item
+          spacing={4}
+          direction="row"
+          alignItems="center"
+          justify="center"
+        >
+          <Grid item>
+            <Card className={classes.card} raised={true}>
+              <CardMedia>
+                <img src={movie} className={classes.media} alt="henlo"></img>
+              </CardMedia>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card className ={classes.cardInfo}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>        </Grid>
+      </Grid>
     </div>
   );
 }
