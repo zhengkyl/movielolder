905a5fb54dae5a1fcf51f6116aa6ec9257429ff0
Kyle Zheng <kylezheng73@gmail.com>
2020-03-02 02:47:18 -0500

    test

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+export default onMovieAdded = functions.database
+.ref('/movies/{movieId}')
+.onCreate((snapshot, context) => {
+    
+})
--- src/components/RankingsScreen.js
+++ src/components/RankingsScreen.js
@@ -1,7 +1,26 @@
 import React from "react";
 
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  makeStyles,
+  // CardActionArea,
+  CardActions,
+  Button
+  //   IconButton,
+  //   Collapse,
+  //   Paper
+} from "@material-ui/core";
+
 function RankingsScreen() {
-  return <div></div>;
+
+  return <div>
+    <Typography variant="h5">
+      Movie Rankinds
+    </Typography>
+  </div>;
 }
 
 export default RankingsScreen;
