d96d2bd46cf6f456023ba4afff1ee3fcf66e45c9
Kyle Zheng <kylezheng73@gmail.com>
2020-03-08 01:26:37 -0500

    clean up old commented out trash

--- src/components/VoteScreen.js
+++ src/components/VoteScreen.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import ContenderCard from "./ContenderCard";
 import { useSpring, animated, interpolate } from "react-spring";
 import { useHover, useDrag } from "react-use-gesture";
-// import movie from "../images/movie.jpg"
 import firebase from "./firebase";
 import { Grid, makeStyles } from "@material-ui/core";
 
@@ -39,37 +38,7 @@ function VoteScreen() {
   const [moviesFetched, setMoviesFetched] = useState(false);
 
   const classes = useStyles();
-  // const [scale1, setScale1] = useState(1);
-  // const [gray1, setGray1] = useState(0);
-  // const bind1 = useHover(({ active }) => {
-  //   if (active) {
-  //     setScale1(1.1);
-  //     setScale2(0.9);
 
-  //     setGray2(90);
-  //   } else {
-  //     setScale1(1);
-  //     setScale2(1);
-
-  //     setGray2(0);
-  //   }
-  // });
-
-  // const [scale2, setScale2] = useState(1);
-  // const [gray2, setGray2] = useState(0);
-  // const bind2 = useHover(({ active }) => {
-  //   if (active) {
-  //     setScale2(1.1);
-  //     setScale1(0.9);
-
-  //     setGray1(90);
-  //   } else {
-  //     setScale2(1);
-  //     setScale1(1);
-
-  //     setGray1(0);
-  //   }
-  // });
   const [currentMovies, setCurrentMovies] = useState([]);
   const [winner, setWinner] = useState(0);
   const vote = () => {};
@@ -96,19 +65,8 @@ function VoteScreen() {
   };
   useEffect(() => fetchMoviesList(), []);
 
-  // const bigBind = useDrag(
-  //   ({ swipe: [swipeX, swipeY] }) => {
-  //     if (winner === 0) {
-  //       setWinner(swipeY);
-  //     }
-  //   },
-  //   { swipeDistance: [30, 30], swipeVelocity: [0.25, 0.25], axis: "y" }
-  // );
-  // Set the drag hook and define component movement based on gesture data
-  // const classes = useStyles();
   return (
     <div
-      // {...bigBind()}
       style={{
         height: `100%`
       }}
@@ -122,31 +80,15 @@ function VoteScreen() {
       >
         {winner >= 0 ? (
           <Grid item container direction="column" alignItems="center">
-            {/* style = {{transform: interpolate([size], s=>`scale(${s})`) }} */}
-            {/* <animated.div
-              // {...bind1()}
-              // style={{
-              //   transform: `scale(${scale1})`,
-              //   filter: `grayscale(${gray1}%)`
-              // }}
-            > */}
             <ContenderCard {...currentMovies[0]}></ContenderCard>
-            {/* </animated.div> */}
           </Grid>
         ) : (
           <div></div>
         )}
         {winner <= 0 ? (
           <Grid item container direction="column" alignItems="center">
-            {/* <animated.div
-              // {...bind2()}
-              // style={{
-              //   transform: `scale(${scale2})`,
-              //   filter: `grayscale(${gray2}%)`
-              // }}
-            > */}
+
             <ContenderCard {...currentMovies[1]}></ContenderCard>
-            {/* </animated.div> */}
           </Grid>
         ) : (
           <div></div>
